b847857fbcd788459e5e57bfe9c1a9c2ccfade9b
Alan Alban <Alan@macbook-pro-3.myfiosgateway.com>
2020-12-27 21:22:37 -0500

    made changes to how skill bars load

--- public/js/skills.js
+++ public/js/skills.js
@@ -1,30 +1,82 @@
 const bars = document.querySelectorAll(".progress-bar");
 
-let i = 0;
-let skill1 = setInterval(function(){
-    i+=1
-    bars[0].style.width = i + "%"
-    if (i >= 70){
-        clearInterval(skill1);
-        i = 0;
-    }
-},15);
+async function loadSkills() {
+    let i = 0;
+    let skill1 =  setInterval(function(){
+        i+=1
+        bars[0].style.width = i + "%"
+        if (i >= 70){
+            clearInterval(skill1);
+            i = 0;
+            let skill2 =  setInterval(function(){
+                i+=1
+                bars[1].style.width = i + "%"
+                if (i >= 50){
+                    clearInterval(skill2);
+                    i = 0;
+                    let skill3 =  setInterval(function(){
+                        i+=1
+                        bars[2].style.width = i + "%"
+                        if (i >= 65){
+                            clearInterval(skill3);
+                            i = 0
+                            let skill4 =  setInterval(function(){
+                                i+=1
+                                bars[3].style.width = i + "%"
+                                if (i >= 50){
+                                    clearInterval(skill4);
+                                    i = 0
+                                    let skill5 =  setInterval(function(){
+                                        i+=1
+                                        bars[4].style.width = i + "%"
+                                        if (i >= 45){
+                                            clearInterval(skill5);
+                                            i=0
+                                        }
+                                    },3);
+                                }
+                            },3);
+                        }
+                    },3);
+                }
+            },3);
+        }
+    },4);
+    
+    // let skill2 =  setInterval(function(){
+    //     i+=1
+    //     bars[1].style.width = i + "%"
+    //     if (i >= 50){
+    //         clearInterval(skill2);
+    //         i = 0;
+    //     }
+    // },19);
+    
+    // let skill3 =  setInterval(function(){
+    //     i+=1
+    //     bars[2].style.width = i + "%"
+    //     if (i >= 65){
+    //         clearInterval(skill3);
+    //     }
+    // },18);
+    
+    // let skill4 =  setInterval(function(){
+    //     i+=1
+    //     bars[3].style.width = i + "%"
+    //     if (i >= 50){
+    //         clearInterval(skill4);
+    //     }
+    // },17);
+    
+    // let skill5 = setInterval(function(){
+    //     i+=1
+    //     bars[4].style.width = i + "%"
+    //     if (i >= 45){
+    //         clearInterval(skill5);
+    //     }
+    // },16);
 
-let skill2 = setInterval(function(){
-    i+=1
-    bars[1].style.width = i + "%"
-    if (i >= 50){
-        clearInterval(skill2);
-        i = 0;
-    }
-},15);
-setTimeout(() => {
-    // wait to execute 
-    }, 1000)
-let skill3 = setInterval(function(){
-    i+=1
-    bars[2].style.width = i + "%"
-    if (i >= 65){
-        clearInterval(skill3);
-    }
-},15);
+}
+
+// Run loaders
+loadSkills()
