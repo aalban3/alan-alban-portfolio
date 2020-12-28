85f82916fb07753946ae88538ec137be30f92b12
Alan Alban <alan.alban10@gmail.com>
2020-12-27 21:45:53 -0500

    testing commit with email linked

--- public/js/skills.js
+++ public/js/skills.js
@@ -42,39 +42,6 @@ async function loadSkills() {
             },3);
         }
     },4);
-    
-    // let skill2 =  setInterval(function(){
-    //     i+=1
-    //     bars[1].style.width = i + "%"
-    //     if (i >= 50){
-    //         clearInterval(skill2);
-    //         i = 0;
-    //     }
-    // },19);
-    
-    // let skill3 =  setInterval(function(){
-    //     i+=1
-    //     bars[2].style.width = i + "%"
-    //     if (i >= 65){
-    //         clearInterval(skill3);
-    //     }
-    // },18);
-    
-    // let skill4 =  setInterval(function(){
-    //     i+=1
-    //     bars[3].style.width = i + "%"
-    //     if (i >= 50){
-    //         clearInterval(skill4);
-    //     }
-    // },17);
-    
-    // let skill5 = setInterval(function(){
-    //     i+=1
-    //     bars[4].style.width = i + "%"
-    //     if (i >= 45){
-    //         clearInterval(skill5);
-    //     }
-    // },16);
 
 }
 
